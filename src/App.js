e3e7e2cc79f34082381f24e5563c529a5356150e
Therese Skaar Bjørknes <thereseskaar@hotmail.com>
2021-03-02 19:13:43 +0100

    add env and procfile for deplayment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import Header from './Components/Header.js';
 import ConverterBox from './Components/ConverterBox.js';
+
 
 function App() {
   return (
     <div className="App">
       <ConverterBox/>
-
     </div>
+
   );
 }
 
