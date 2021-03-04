380fd6d81d499ec960d28551ef966f4ec8e6f902
Therese Skaar Bjørknes <thereseskaar@hotmail.com>
2021-03-04 16:30:02 +0100

    update server.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import {Route} from 'react-router-dom';
 
-const express = require("express");
-const app = express();
 
 ReactDOM.render(
   <BrowserRouter>
@@ -18,7 +16,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-app.listen(process.env.PORT||3000)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
