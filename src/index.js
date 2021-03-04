e3b68f069ebce5c30bf4f530742d6095e52d34d6
Therese Skaar Bjørknes <thereseskaar@hotmail.com>
2021-03-04 15:20:07 +0100

    add node/express and set port

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
 import {Route} from 'react-router-dom';
 
+const express = require("express");
+const app = express();
+
 ReactDOM.render(
   <BrowserRouter>
   <React.StrictMode>
@@ -14,6 +17,8 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
+
+app.listen(process.env.PORT||3000)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
